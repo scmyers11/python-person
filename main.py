e9e5b33b721ab5aba4546f95d3eb22e9975d9738
Stephen Myers <scmyers11@gmail.com>
2017-01-30 14:25:08 -0600

    Adds default layout

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import webapp2
 import person
 
-class Index(webapp2.RequestHandler):
+page_layout = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Who I Am</title>
+</head>
+<body>
+    <h1>
+        <a href="/">Who Am I?</a>
+    </h1>
+    <div class="content">
+        %(content)s
+    </div>
+</body>
+</html>
+"""
+
+class Handler(webapp2.RequestHandler):
+    def renderContent(self, content=""):
+        self.response.write(page_layout % { "content" : content })
+
+class Index(Handler):
     def get(self):
         me = person.Person("Stephen Myers", ["scmyers11@gmail.com", "stephen@impresscareer.com"], "(636) 368-5639")
-        self.response.write(str(me))
+        self.renderContent(str(me))
 
 app = webapp2.WSGIApplication([
     ('/', Index)
